09ea0c28d13755cc8924e87aecadd514f8fc8ca9
Samir Azzam <148980040+LynxxHub@users.noreply.github.com>
2023-11-03 22:28:17 +0200

    Integrated the HTML template into application's _Layout

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewDesignTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string MyCustomText { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+		public void OnPost()
+		{
+            MyCustomText = "Hello World";
+		}
+	}
+}
